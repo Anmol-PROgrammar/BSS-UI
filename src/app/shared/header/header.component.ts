a03d5ca62123aabe48eac96a6315643cf038a3f5
Anmol Rawat <gehuanmolrawat@gmail.com>
2026-01-31 05:27:12 +0530

    home-about-header-quickAccssBtn comp refactored to responsive design

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
-import { AppRoutingModule } from '../../app-routing.module';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../../app-routing.module';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
-  imports: [AppRoutingModule, CommonModule],
+  imports: [CommonModule, AppRoutingModule],
 })
 export class HeaderComponent {
-  isMenuOpen: boolean = false;
+  isMenuOpen = false;
 
   constructor(private router: Router) {}
 
   navigateToHome() {
     this.router.navigate(['/home']);
+    this.isMenuOpen = false;
   }
 
   toggleNavLinks() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
   }
 }
